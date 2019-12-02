00bbb352e0ec21121fd725511eee6bec09d582a0
Jazzy <jrbbp7@mail.missouri.edu>
2019-12-02 16:30:52 -0600

    first commit

---
+++ Soulstice.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Soulstice.UI.MVC.MvcApplication" Language="C#" %>
---
+++ Soulstice.UI.MVC/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace Soulstice.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
